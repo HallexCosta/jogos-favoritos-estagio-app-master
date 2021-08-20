b95c969d5f4dfa337d74affc1e0b59d1c58824fc
HallexCosta <hallex.costa@hotmail.com>
2021-08-19 21:23:34 -0300

    create a list of comments with possible Steam API's

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const routes = Router();
 
+/*
+DEV API - THIS API IS PAY
+http://api.steampowered.com/<interface name>/<method name>/v<version>/?key=<api key>&format=<format>
+
+DEV API - FREE
+https://api.steampowered.com/ISteamApps/GetAppList/v2/
+
+LIST ALL APPS
+http://api.steampowered.com/ISteamApps/GetAppList/v0002/?key=STEAMKEY&format=json
+
+LIST APP DETAILS
+http://store.steampowered.com/api/appdetails?appids={APP_ID}
+*/
+
 routes.get("/", async (request: Request, response: Response) => {
   return response.json({
     ok: true,
