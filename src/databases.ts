86b0a6d9ab16e552d951d6f1a1cbaa410d6c39d8
HallexCosta <hallex.costa@hotmail.com>
2021-08-20 01:27:42 -0300

    add method findByGameId and fix method save

--- src/databases.ts
+++ src/databases.ts
@@ -1,11 +1,39 @@
 export type Favorite = {
   grade: number;
-  gameId: number;
-  userHash: string;
+  game_id: number;
+  user_hash: string;
 };
 
-export class FavoriteDatabase extends Map {
+export class FavoriteDatabase extends Map<string, Favorite[]> {
   public save(userHash: string, favorite: Favorite) {
-    this.set(userHash, favorite);
+    const favorites = this.get(userHash);
+
+    if (favorites) {
+      this.set(userHash, [...favorites, favorite]);
+    }
+
+    if (!favorites) {
+      this.set(userHash, [favorite]);
+    }
+  }
+
+  public findByGameId(userHash: string, gameId: number) {
+    const favorites = this.get(userHash);
+
+    return favorites.find((favorite) => favorite.game_id === gameId);
+  }
+
+  public deleteByGameId(userHash: string, gameId: number) {
+    const favorites = this.get(userHash);
+
+    const temp = [];
+
+    for (const favorite of favorites) {
+      if (favorite.game_id !== gameId) {
+        temp.push(favorite);
+      }
+    }
+
+    this.set(userHash, temp);
   }
 }
