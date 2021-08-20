7ebd0114129cc35a9d102d8ccc2f221963a5c851
HallexCosta <hallex.costa@hotmail.com>
2021-08-19 21:40:12 -0300

    add filter by game title in GET route "/"

--- src/server.ts
+++ src/server.ts
@@ -40,13 +40,25 @@ routes.get("/", async (request: Request, response: Response) => {
   // BODY: título do jogo
   // Filtrar jogos usando título do jogo
 
+  const title = request.query.title as string;
+
   const { data } = await api.get<SteamAPIResponse>(
     "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
   );
 
   const { apps: games } = data.applist;
 
-  return response.json(games);
+  function searchGameByTitle(gameName: string, gameTitle: string) {
+    const regex = new RegExp(gameTitle, "gi");
+
+    return gameName.match(regex);
+  }
+
+  const filteredGames = games.filter((game) =>
+    searchGameByTitle(game.name, title)
+  );
+
+  return response.json(filteredGames);
 });
 
 app.use(routes);
