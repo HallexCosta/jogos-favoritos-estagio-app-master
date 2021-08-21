0d8bdc1769f014dc609599b973c438f7edfbfebe
HallexCosta <hallex.costa@hotmail.com>
2021-08-21 15:49:20 -0300

    refactor server to separate routes from app

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Router, Request, Response } from "express";
+
+import { routes } from "./routes";
+
+export const app = express();
+
+export const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.use(routes);
