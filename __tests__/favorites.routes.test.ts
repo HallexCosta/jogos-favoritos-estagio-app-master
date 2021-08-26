db560102d05487ab851d8a69297aa2357879476d
HallexCosta <hallex.costa@hotmail.com>
2021-08-26 16:03:36 -0300

    change status return 403 to 409 on endpoint POST "/favorite"

--- __tests__/favorites.routes.test.ts
+++ __tests__/favorites.routes.test.ts
@@ -6,7 +6,7 @@ import { app } from "../src/app";
 const agent = supertest(app);
 
 (async () => {
-  console.log("> Run test on routes /favorites");
+  console.log("> Run test on routes /favorite");
 
   const fakeFavoriteGame = {
     rating: 5,
@@ -18,12 +18,12 @@ const agent = supertest(app);
   // should be able to throw an error if try favorited a game without "user-hash" sending by header
   {
     agent
-      .post("/favorites")
+      .post("/favorite")
       .send({
         ...fakeFavoriteGame,
         rating: 10,
       })
-      .expect(409)
+      .expect(403)
       .end((error) => {
         if (error) throw error;
 
@@ -37,7 +37,7 @@ const agent = supertest(app);
   // should be able to throw error if rate is less than zero and greater than five
   {
     agent
-      .post("/favorites")
+      .post("/favorite")
       .set("user-hash", fakeUserHash)
       .send({
         ...fakeFavoriteGame,
@@ -57,7 +57,7 @@ const agent = supertest(app);
   // should be able allow user to rate a game from zero to five.
   {
     agent
-      .post("/favorites")
+      .post("/favorite")
       .set("user-hash", fakeUserHash)
       .send({ ...fakeFavoriteGame, game_id: 1020710, rating: 0 })
       .then(({ body: favoriteGame }) => {
@@ -67,7 +67,7 @@ const agent = supertest(app);
       });
 
     agent
-      .post("/favorites")
+      .post("/favorite")
       .set("user-hash", fakeUserHash)
       .send({ ...fakeFavoriteGame, game_id: 10, rating: 5 })
       .then(({ body: favoriteGame }) => {
@@ -85,7 +85,7 @@ const agent = supertest(app);
   // should be able to add in memory new favorite game by user hash
   {
     agent
-      .post("/favorites")
+      .post("/favorite")
       .send({ fakeFavoriteGame, login: "testing", game_id: 585280 })
       .then(({ body: favoriteGame }) => {
         assert(favoriteGame);
@@ -99,7 +99,7 @@ const agent = supertest(app);
   // should be able to list favorite games by user hash
   {
     agent
-      .get("/favorites")
+      .get("/favorite")
       .send()
       .set("user-hash", fakeUserHash)
       .then(({ body: [favoriteGame] }) => {
@@ -111,7 +111,7 @@ const agent = supertest(app);
   // should be able not throw error if trying to disfavor a game not added by user hash
   {
     agent
-      .delete(`/favorites/${fakeFavoriteGame.game_id}`)
+      .delete(`/favorite/${fakeFavoriteGame.game_id}`)
       .set("user-hash", "FAKE USER HASH")
       .expect(409)
       .end((error) => {
@@ -127,7 +127,7 @@ const agent = supertest(app);
   // should be able to delete a favorite game by id from user hash
   {
     agent
-      .delete(`/favorites/10`)
+      .delete(`/favorite/10`)
       .send()
       .set("user-hash", fakeUserHash)
       .expect(200)
@@ -144,7 +144,7 @@ const agent = supertest(app);
   // should be able to return 204 and empty array if "user-hash" not have none game added
   {
     agent
-      .get(`/favorites`)
+      .get(`/favorite`)
       .set("user-hash", fakeUserHash)
       .expect(200)
       .end((error) => {
